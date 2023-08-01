0d6609ec73f7304c6c8e6458994f27cc78d159a4
judiciousmurich <murichjudicious45@gmail.com>
2023-08-01 16:07:58 +0300

    chore:add navbar icons

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,23 +1,27 @@
 import { NavLink } from "react-router-dom";
+import { BiSolidHomeCircle } from "react-icons/bi";
+import { FaCar, FaHistory, FaPlus, FaUserClock } from "react-icons/fa";
+import { LuMessagesSquare} from "react-icons/lu";
+import { PiBellSimpleZ} from "react-icons/pi";
 
 const Navbar: React.FC = () => {
   return (
     <div className="flex justify-between bg-[#060b20] text-[white] p-6">
       <div className="flex gap-[4rem]">
         <div>
-          <img src="" alt="LOGO" />
+          <img src="" alt="VROOM" />
         </div>
         <div className="flex gap-6">
-          <NavLink to="home">Home</NavLink>
-          <NavLink to="cars">Cars</NavLink>
-          <NavLink to="dealers">Dealers</NavLink>
-          <NavLink to="messages">Message</NavLink>
-          <NavLink to="history">History</NavLink>
+          <NavLink className="flex items-center gap-2" to="home"><BiSolidHomeCircle/>Home</NavLink>
+          <NavLink  className="flex items-center gap-2" to="cars"><FaCar/>Cars</NavLink>
+          <NavLink  className="flex items-center gap-2" to="dealers"><FaUserClock/>Dealers</NavLink>
+          <NavLink  className="flex items-center gap-2" to="messages"><LuMessagesSquare/>Message</NavLink>
+          <NavLink  className="flex items-center gap-2" to="history"><FaHistory/>History</NavLink>
         </div>
       </div>
-      <div className="flex gap-4">
-        <p>+</p>
-        <p>Bell</p>
+      <div className="flex gap-4 items-center">
+        <p className="bg-[#3d903b] text-white rounded-full p-3"><FaPlus/></p>
+        <p><PiBellSimpleZ/></p>
         <p>Profile</p>
       </div>
     </div>

3c3e42b5f7c943bb809e2ab044579435d08e87a4
judiciousmurich <murichjudicious45@gmail.com>
2023-08-04 11:44:11 +0300

    feat:Update PopularCar and PopularCars components with new props implementation

--- src/components/cars/PopularCars.tsx
+++ src/components/cars/PopularCars.tsx
@@ -1,13 +1,48 @@
-import PopularCar from "./PopularCar"
+import PopularCar from "./PopularCar";
 
 const PopularCars = () => {
+  const popularCars = [
+    {
+      carBrandImg: "",
+      carImg: "",
+      carName: "Porsche 911",
+      carModel: "Turbo S - 2021",
+      amountPday: "",
+      ownerName: "Judicious Murich",
+      ownerImg: "",
+      bgColor: "",
+      bg2Color: "",
+    },
+    {
+      carBrandImg: "",
+      carImg: "",
+      carName: "Nissan GT-R-2023",
+      carModel: "Nismo 3.8 V6",
+      amountPday: "",
+      ownerName: "Amos Mwongela",
+      ownerImg: "",
+      bgColor: "",
+      bg2Color: "",
+    },
+    {
+      carBrandImg: "",
+      carImg: "",
+      carName: "Chevrolet C8",
+      carModel: "Corvette Stringray",
+      amountPday: "",
+      ownerName: "Joshua Musira",
+      ownerImg: "",
+      bgColor: "",
+      bg2Color: "",
+    },
+  ];
   return (
     <div className="grid grid-cols-3 gap-[1.5rem] md:flex md:justify-center md:borde">
-        <PopularCar/>
-        <PopularCar/>
-        <PopularCar/>
+      {popularCars.map((car) => (
+        <PopularCar carInfo={car} />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default PopularCars
+export default PopularCars;

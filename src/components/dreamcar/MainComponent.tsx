a5d2d5001a5c2e077a042252764cb796d495f539
mwongess <amosmwongelah@gmail.com>
2023-08-10 11:14:37 +0300

    fix breaking styles

--- src/components/dreamcar/MainComponent.tsx
+++ src/components/dreamcar/MainComponent.tsx
@@ -4,14 +4,12 @@ import CarComponent from '../dreamcar/CarComponent';
 
 const MainComponent: React.FC = () => {
   return (
-    <div className="justify-center items-center  ">
-      <CarComponent
-        carBrand ="Porsche"
-        carModel="Any"
-        pickupLocation="Judicious M. Murich Airport"
-        pickupDateAndReturnDate="Apr 27 - May 1"
-      />
-    </div>
+    <CarComponent
+      carBrand="Porsche"
+      carModel="Any"
+      pickupLocation="Judicious M. Murich Airport"
+      pickupDateAndReturnDate="Apr 27 - May 1"
+    />
   );
 };
 

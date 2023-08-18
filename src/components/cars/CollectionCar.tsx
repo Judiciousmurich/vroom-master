8bcca04ea05116e38c5a0c288b92034dd1e7c941
judiciousmurich <murichjudicious45@gmail.com>
2023-08-18 17:41:23 +0300

    feat:add ourCollection

--- src/components/cars/CollectionCar.tsx
+++ src/components/cars/CollectionCar.tsx
@@ -1,13 +1,15 @@
-const CollectionCar = () => {
+import React from 'react';
+
+const CollectionCar: React.FC = () => {
   return (
     <div className="rounded">
       <h1>Our Collection</h1>
       <div>
-            <h1 className="font-bold">911 Carrera GTS</h1>
-            <p className=" text-[#999999]">Type 992</p>
-          </div>
+        <h1 className="font-bold">911 Carrera GTS</h1>
+        <p className="text-[#999999]">Type 992</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CollectionCar
+export default CollectionCar;

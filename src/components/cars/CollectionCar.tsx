c4c237d7ea4baa26449df703fc1b92c241486904
judiciousmurich <murichjudicious45@gmail.com>
2023-10-19 22:23:22 +0300

    feat:add styles

--- src/components/cars/CollectionCar.tsx
+++ src/components/cars/CollectionCar.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const CollectionCar: React.FC = () => {
   return (
     <div className="rounded">
-      <h1>Our Collection</h1>
+      <h1 className='font-bold capitalize'>Our Collection</h1>
       <div>
         <h1 className="font-bold">911 Carrera GTS</h1>
         <p className="text-[#999999] font-bold text-base" >Type 992</p>

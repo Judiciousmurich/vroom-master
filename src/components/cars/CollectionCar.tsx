44e8fc5f6027f2acf6892ccc38f3bc39bebc1c14
judiciousmurich <murichjudicious45@gmail.com>
2023-10-26 17:20:19 +0300

    feat:add colllection car styles

--- src/components/cars/CollectionCar.tsx
+++ src/components/cars/CollectionCar.tsx
@@ -1,13 +1,35 @@
 
-const CollectionCar = ({ carInfo }: { carInfo: {  carImg: string, carName: string, carModel: string, amountPday: string,  } }) => {
+const CollectionCar = ({ carInfo }: { carInfo: { carImg: string, carName: string, carModel: string, amountPday: string, bgColor: string, bg2Color: string } }) => {
   return (
-    <div className="rounded">
-      <h1 className='font-bold capitalize'>Our Collection</h1>
-      <div>
-        <h1 className="font-bold">911 Carrera GTS</h1>
-        <p className="text-[#999999] font-bold text-base" >Type 992</p>
+    <div className={`rounded-lg w-full ${carInfo.bgColor}`}>
+      <div className="flex justify-between p-3">
+        <div className="flex gap-3 items-center">
+
+          <div>
+            <h1 className="font-bold">{carInfo.carName}</h1>
+            <p>{carInfo.carModel}</p>
+          </div>
+        </div>
+        <div>
+          <p className="bg-white rounded-lg px-3 py-1 whitespace-nowrap" >2 Units available</p>
+        </div>
+      </div>
+      <div className="flex justify-center ">
+        <img className="h-[10rem]" src={carInfo.carImg} alt="car" />
+      </div>
+      <div className={`flex justify-between p-3 ${carInfo.bg2Color}`}>
+        <div className="flex gap-2">
+
+
+        </div>
+        <div>
+          <h1 className="font-bold sm:text-xl">
+            $950<span className="font-normal">/day</span>
+          </h1>
+        </div>
       </div>
     </div>
+
   );
 };
 
